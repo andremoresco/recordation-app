c41303858036c44eda608b5530044e9c80adbb8e
Andre Moresco <amoresco1@gmail.com>
2021-07-15 21:06:21 -0300

    Fixed and added new test case to verefy user authorities

--- user-management-service/src/test/java/com/recordation/usermanagementservice/userCases/createUser/CreateUserControllerTest.java
+++ user-management-service/src/test/java/com/recordation/usermanagementservice/userCases/createUser/CreateUserControllerTest.java
@@ -8,11 +8,14 @@ import com.recordation.usermanagementservice.useCases.createUser.CreateUserUseCa
 import com.recordation.usermanagementservice.useCases.findUserByIdentifier.FindUserByIdentifierUseCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -40,9 +43,12 @@ public class CreateUserControllerTest {
     @MockBean
     private CreateUserUseCase createUserUseCase;
 
-    @Test
-    public void handleCreateUserOk() throws Exception {
+    @MockBean
+    private PermissionEvaluator proxyTest;
 
+    @Test
+    @WithMockUser(authorities = "ADMIN")
+    public void handleCreateUserOk() throws Exception {
         CreateUserDTO createUserDTO = CreateUserDTO.builder()
                 .name("Andre")
                 .userIdentifier("08797124907")
@@ -58,6 +64,7 @@ public class CreateUserControllerTest {
     }
 
     @Test
+    @WithMockUser(authorities = "ADMIN")
     public void handleCreateUserContentNull() throws Exception {
         this.mockMvc.perform(
                 MockMvcRequestBuilders.post("/users")
@@ -67,6 +74,7 @@ public class CreateUserControllerTest {
     }
 
     @Test
+    @WithMockUser(authorities = "ADMIN")
     public void handleCreateUserContentMissingNameInformation() throws Exception {
         CreateUserDTO createUserDTO = CreateUserDTO.builder()
                 .userIdentifier("08797124907")
@@ -82,5 +90,22 @@ public class CreateUserControllerTest {
                 .andDo(print())
                 .andExpect(status().isBadRequest())
                 .andExpect(content().json("{\"error_code\": \"METHOD_ARGUMENT_NOT_VALID\", \"message\":[\"Name is required!\"]}"));
+    }
+
+    @Test
+    @WithMockUser(authorities = "USER")
+    public void handleCreateUserWithoutAdminAuthority() throws Exception {
+        CreateUserDTO createUserDTO = CreateUserDTO.builder()
+                .name("Andre")
+                .userIdentifier("08797124907")
+                .email("teste@unit.com")
+                .password("testunit")
+                .build();
+        this.mockMvc.perform(
+                MockMvcRequestBuilders.post("/users")
+                        .content(new Gson().toJson(createUserDTO))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isForbidden());
     }
 }
